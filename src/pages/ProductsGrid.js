2df1a89992749583e1dbda330a4140df2a535abb
Deloz09 <84214640+TheDeloz-v2@users.noreply.github.com>
2023-04-21 17:25:17 -0600

    ProductsGrid 3.0
    Implementation of search bar, without functionality.

--- src/pages/ProductsGrid.js
+++ src/pages/ProductsGrid.js
@@ -1,11 +1,29 @@
 import React from "react";
 import { Grid, Box} from "@mui/material";
 import ProductCard from "./ProductCard";
+import PrimarySearchAppBar from "../components/PrimarySearchAppBar";
+import { SpaceBar } from "@mui/icons-material";
 
 const ProductsGrid = ({ products }) => {
   return (
-    <Box textAlign="left">
-        <h1 style={{ marginLeft: "50px", marginBottom:"30px", marginTop:"30px" }}>Nuestros Productos</h1>
+    <Grid spacing={2}>
+        <Grid
+            container
+            direction="row"
+            justifyContent="space-evenly"
+            alignItems="center"
+            textAlign="left">
+
+            <item xs={8} textAlign="left" >  
+                <h1 style={{ marginLeft: "50px", marginBottom:"30px", marginTop:"30px", textAlign:"left"}}>Nuestros Productos</h1>
+            </item>
+
+            <item xs={4}>  
+                <PrimarySearchAppBar/>
+            </item>
+
+        </Grid>
+
     <Grid container spacing={1}>
       {products.map((product) => (
         <Grid
@@ -25,7 +43,7 @@ const ProductsGrid = ({ products }) => {
         </Grid>
       ))}
     </Grid>
-    </Box>
+    </Grid>
   );
 };
 
