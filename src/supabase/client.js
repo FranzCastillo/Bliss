36e19f515e4349317ec399b00d35eb25fe11f2b4
Mendezg1 <eurocoparich@gmail.com>
2023-05-07 23:10:38 -0600

    Supabase y correccion client.js
    Se corrigió el archivo 'client.js' de exportar una constante a una funcion de conexion

--- src/supabase/client.js
+++ src/supabase/client.js
@@ -1,9 +1,10 @@
-const {createClient} = require("@supabase/supabase-js");
+import {createClient} from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.REACT_APP_DB_URL;
 const supabaseKey = process.env.REACT_APP_DB_KEY;
 
 console.log(process.env.REACT_APP_DB_URL);
 console.log( process.env.REACT_APP_DB_KEY);
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export default supabase;
