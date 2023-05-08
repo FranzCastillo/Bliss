f2b97d200b4cfaefa869456f450989a3ae0a932a
Mendezg1 <eurocoparich@gmail.com>
2023-05-07 23:17:44 -0600

    Modelo utilización fetchAPI
    Se proveyó un modelo (probado) para utilizar la API creada anteriormente

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,25 @@
 import React from 'react'
+import {useEffect, useState} from 'react'
+import fetchTable from '../components/fetchAPI.js'
+/*
+      -- Modelo para usar fetchTable --
+
+    const [datos, setDatos] = useState(null)
+    useEffect(() => {
+      const fetch = async () =>{
+        setDatos( await fetchTable('tipos_de_pago') ) 
+      }
+      fetch()
+    }, [])
+    |---------------------------------------------------|
+    -- Una forma simple de imprimir los datos --
+    {datos && (
+        <div className='si'>
+          {datos.map(dato =>(<p>{dato.tipo}</p>))}
+        </div>
+      )}
+    |---------------------------------------------------|
+  */
 
 function Home() {
   return (
