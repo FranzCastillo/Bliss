620fa61224a5fe33dc737d36a842bd83e20cdec2
Francisco Castillo <cas21562@uvg.edu.gt>
2023-05-11 16:45:11 -0600

    Start in Dev mode

--- src/components/NavBarUser.js
+++ src/components/NavBarUser.js
@@ -28,7 +28,6 @@ function NavBarUser() {
         try {
             navigate("/login")
            await supabase.auth.signOut();
-           //
         } catch (error) {
             console.log('Error signing out:', error.message);
         }
