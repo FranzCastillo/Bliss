c849bb604298cda07c47c815439f4c6858513f3b
Mendezg1 <eurocoparich@gmail.com>
2023-08-08 19:32:36 -0600

    test fetchProds

---
+++ src/pages/__tests__/FetchProductsWorks.test.js
@@ -0,0 +1,9 @@
+import { FetchProducts } from "../../fetchProducts";
+
+test('should fetch products', async () =>{
+    const CantidadDeProductosActual = 9
+    const prods = await FetchProducts()
+    expect(prods.length).toBe(CantidadDeProductosActual)
+});
+
+  
