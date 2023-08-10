becd52262172032dbfdb8ac70b856d1d47511786
Fernanda Esquivel <80931098+FerEsq@users.noreply.github.com>
2023-08-09 22:18:52 -0600

    Update ProductCard.jsx

--- src/pages/ProductCard.jsx
+++ src/pages/ProductCard.jsx
@@ -31,67 +31,33 @@ const ProductCard = ({ product }) => {
 
     return (
         <Card sx={{ width: '250px', height: '100%' }}>
-            <Card className='img-card' sx={{ boxShadow: 'none' }}>
-                <CardMedia
-                    component="img"
-                    className='product-img'
-                    sx={{ objectFit: 'contain' }}
-                    image={imageURL}
-                    alt={product.name}
-                    onClick={handleProductClick}
-                />
-            </Card>
+            <br/>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <Card className='img-card' sx={{ boxShadow: 'none', width: '200px' }}>
+                    <CardMedia
+                        component="img"
+                        className='product-img'
+                        sx={{ objectFit: 'contain' }}
+                        image={imageURL}
+                        alt={product.name}
+                        onClick={handleProductClick}
+                    />
+                </Card>
+            </div>
+            <br/>
             <CardContent>
                 <Typography gutterBottom variant="h6" component="div" align='left'>
-                    {product.name}
-                    <FormControl >
-                        <FormLabel  id="demo-radio-buttons-group-label" style={{ fontSize: '14px', color: '#201B40' }} >
-                            Talla
-                        </FormLabel>
-                        <RadioGroup
-                            row
-                            aria-labelledby="demo-radio-buttons-group-label"
-                            defaultValue={product.sizes[1]}
-                            name="radio-buttons-group"
-                            style={{ fontSize: '12px' }} 
-                            onChange={handleSelectSize}
-                        >
-                        {product.sizes.slice(1).map((talla) => (
-                            <FormControlLabel
-                                key={talla}
-                                value={talla}
-                                control={<Radio 
-                                    sx={{
-                                        '& .MuiSvgIcon-root': {
-                                        fontSize: 18,
-                                        color: '#201B40',
-                                        },
-                                    }}
-                                
-                                />}
-                                label={talla.toString()}
-                            />
-                        ))}
-
-                        </RadioGroup>
-                </FormControl>
+                    {product.name}    
                 </Typography>
-                
+                <Typography gutterBottom variant="subtitle2" component="div" align='left'>
+                    {product.detail}
+                </Typography>
                 <Typography variant="body2" color="text.secondary" align='right'>
                     {product.code}
                 </Typography>
                 <Typography variant="h6" color="text.primary" align='right'>
                     Q{product.price}
                 </Typography>
-                <br />
-                <Button
-                    variant="contained"
-                    sx={{ backgroundColor: '#312D4F' }}
-                    onClick={handleAddToCart}
-                >
-                    Agregar al carrito
-                    <AddShoppingCartIcon />
-                </Button>
             </CardContent>
         </Card>
     );
