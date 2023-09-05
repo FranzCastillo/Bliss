478a048e0a0ffa4985290b29d9c54d104d2f46e0
Francisco Castillo <castillocerna@gmail.com>
2023-09-04 21:37:30 -0600

    Descriptive test name

--- src/__vitest__/userExperience.test.jsx
+++ src/__vitest__/userExperience.test.jsx
@@ -2,9 +2,8 @@ const { Builder } = require("selenium-webdriver");
 import {describe, test, expect} from 'vitest'
 const { login_test, product_navigation_test } = require("./SeleniumTests/selenium.cjs");
 
-const driver = new Builder().forBrowser("chrome").build();
 
-describe("Login Test", () => {
+describe("UX Tests", () => {
     test("should log in successfully", async () => {
         const {passed, error} =  await login_test();
         if (error !== null) {
