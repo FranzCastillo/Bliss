d86bbcfe369173bb320461ffbfc20e7f453b53cb
Mendezg1 <eurocoparich@gmail.com>
2023-11-12 21:47:19 -0600

    basicLoad documented

--- src/__k6test__/load/basicLoad.js
+++ src/__k6test__/load/basicLoad.js
@@ -1,7 +1,7 @@
-import http from 'k6/http';
+import http from 'k6/http'; 
 import { sleep } from 'k6';
 
 export default function () {
-  http.get('https://bliss-three.vercel.app/login');
+  http.get('https://bliss-three.vercel.app/login'); //Se establece el URL de la página a realizar el test.
   sleep(1);
 }
