409a0843b381dbab228148b18a8e046390a59dd5
Mendezg1 <eurocoparich@gmail.com>
2023-11-12 21:46:28 -0600

    personalizedChargeLoad documented

--- src/__k6test__/load/personalizedChargeLoad.js
+++ src/__k6test__/load/personalizedChargeLoad.js
@@ -2,11 +2,11 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export let options = {
-  vus: 10, // Usuarios virtuales simultáneos
-  duration: '30s', // Duración de la prueba
+  vus: 10, // Se establecen los usuarios simultáneos de la evaluación
+  duration: '30s', // Tiempo de duración de la prueba
 };
 
 export default function () {
-    http.get('https://bliss-three.vercel.app/login');
+    http.get('https://bliss-three.vercel.app/login'); // Se establece la URL de la página a la cual realizar los tests.
   sleep(1);
 }
