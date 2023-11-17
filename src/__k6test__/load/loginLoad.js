56111b4ea33d1e76f8f950f9a2fb1ba00178b5df
Mendezg1 <eurocoparich@gmail.com>
2023-11-17 13:44:10 -0600

    test documentation

--- src/__k6test__/load/loginLoad.js
+++ src/__k6test__/load/loginLoad.js
@@ -3,20 +3,21 @@ import { sleep } from 'k6';
 
 export let options = {
   stages: [
-    { duration: '30s', target: 10 }, // 10 usuarios durante 30 segundos
-    { duration: '1m', target: 20 }, // 20 usuarios durante 1 minuto
-    { duration: '30s', target: 0 }, // Desaceleración: 0 usuarios durante 30 segundos
+    { duration: '30s', target: 10 }, // 10 users in 30s
+    { duration: '1m', target: 20 }, // 20 users in 1 min
+    { duration: '30s', target: 0 }, // 0 users in 30s
   ],
 };
 
 export default function () {
+  //Makes a POST request to the web page with parameters to make a log in.
   const response = http.post('https://bliss-three.vercel.app/', {
     email: "cas21562@uvg.edu.gt",
     password: "cas21562",
   });
 
   http.get('https://bliss-three.vercel.app/', {
-    cookies: { token: response.cookies.token }, // Utiliza la cookie de autenticación
+    cookies: { token: response.cookies.token }, // Uses the auth cookie
   });
 
   sleep(1);
